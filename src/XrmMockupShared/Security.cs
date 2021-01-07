4aff8706ac00006794fb1ae8cc3f25bd680a447d
Matt Trinder <matt.trinder@tisski.com>
2021-01-07 08:40:49 +0000

    Merge branch '127_add_role_templates_to_db' into bring_all_together
    # Conflicts:
    #	src/MetadataShared/DataHelper.cs
    #	src/MetadataSkeleton/MetadataSkeleton.cs
    #	src/XrmMockupShared/Security.cs
    #	src/XrmMockupShared/Utility.cs
    #	tests/SharedTests/TestSecurityRoles.cs
    #	tests/XrmMockup11Test/Metadata/AdditionalMetadata.xml
    #	tests/XrmMockup13Test/Metadata/AdditionalMetadata.xml
    #	tests/XrmMockup15Test/Metadata/AdditionalMetadata.xml
    #	tests/XrmMockup16Test/Metadata/AdditionalMetadata.xml
    #	tests/XrmMockup365Test/Metadata/AdditionalMetadata.xml

--- src/XrmMockupShared/Security.cs
+++ src/XrmMockupShared/Security.cs
@@ -46,6 +46,8 @@ namespace DG.Tools.XrmMockup
 
         internal void AddRolesForBusinessUnit(XrmDb db, EntityReference businessUnit)
         {
+            AddRoleTemplatesForBusinessUnit(db, businessUnit);
+
             foreach (var sr in SecurityRoles.Values)
             {
                 var roleMeta = Metadata.EntityMetadata.GetMetadata("role");
@@ -56,7 +58,10 @@ namespace DG.Tools.XrmMockup
                 role[roleMeta.PrimaryIdAttribute] = role.Id;
                 role["businessunitid"] = businessUnit;
                 role["name"] = sr.Name;
-                role["roletemplateid"] = sr.RoleTemplateId;
+                if (sr.RoleTemplateId != Guid.Empty)
+                {
+                    role["roletemplateid"] = new EntityReference("roletemplate", sr.RoleTemplateId);
+                }
                 role["createdby"] = Core.AdminUserRef;
                 role["createdon"] = DateTime.UtcNow.Add(Core.TimeOffset);
                 role["modifiedby"] = Core.AdminUserRef;
@@ -83,6 +88,23 @@ namespace DG.Tools.XrmMockup
             role["modifiedon"] = DateTime.UtcNow.Add(Core.TimeOffset);
             db.Add(role);
             SecurityRoleMapping.Add(role.Id, newRole.RoleId);
+        }
+        internal void AddRoleTemplatesForBusinessUnit(XrmDb db, EntityReference businessUnit)
+        {
+            //role templates are actually uniques across all business units so don't try adding more than once.
+            var allRoleTemplates = db.GetDBEntityRows("roletemplate").Select(x => x.ToEntity());
+
+            foreach (var sr in SecurityRoles.Values.Where(x => x.RoleTemplateId != Guid.Empty).GroupBy(x => x.RoleTemplateId).Select(x => x.Key))
+            {
+                if (!allRoleTemplates.Any(x => x.Id == sr))
+                {
+                    var roleTemplate = new Entity("roletemplate")
+                    {
+                        Id = sr
+                    };
+                    db.Add(roleTemplate);
+                }
+            }
         }
 
         internal void InitializeSecurityRoles(XrmDb db)
