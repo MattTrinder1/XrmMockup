eb25d38168f7e3bd525b5025bf64d0552733a729
Matt Trinder <matttrinder@users.noreply.github.com>
2024-03-14 12:38:59 +0000

    add AccessTeamTemplates

--- src/MetadataSkeleton/MetadataSkeleton.cs
+++ src/MetadataSkeleton/MetadataSkeleton.cs
@@ -14,6 +14,7 @@ namespace DG.Tools.XrmMockup {
         public List<MetaPlugin> Plugins;
         public OptionSetMetadataBase[] OptionSets;
         public Dictionary<string, Dictionary<int,int>> DefaultStateStatus;
+        public List<Entity> AccessTeamTemplates;
 
         public void Merge(MetadataSkeleton metadata)
         {
