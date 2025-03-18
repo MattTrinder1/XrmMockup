d050fceb925a41f79f8bf598698a84f75512fc11
Matt Trinder <matttrinder@users.noreply.github.com>
2025-03-18 21:04:53 +0000

    impleement PublishXmlRequest

---
+++ src/XrmMockupShared/Requests/PublishXmlRequestHandler.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Crm.Sdk.Messages;
+using DG.Tools.XrmMockup.Database;
+using System;
+using System.Linq;
+
+namespace DG.Tools.XrmMockup
+{
+    internal class PublishXmlRequestHandler : RequestHandler {
+        internal PublishXmlRequestHandler(Core core, XrmDb db, MetadataSkeleton metadata, Security security) : base(core, db, metadata, security, "PublishXml") {}
+
+        internal override OrganizationResponse Execute(OrganizationRequest orgRequest, EntityReference userRef) {
+            var request = MakeRequest<PublishXmlRequest>(orgRequest);
+            
+            var ret = new PublishXmlResponse();
+            ret.Results = new ParameterCollection();
+            return ret;
+        }
+    }
+}
